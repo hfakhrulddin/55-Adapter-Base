7f39849272f59e4b588b5320df57b9e8ea04b6c3
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-04 13:11:21 -0700

    inint

---
+++ Adapter-Base/Adapter-Base/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Adapter_Base
+{
+    static class Adapter
+    {
+        static Adapter()
+        {
+            Console.WriteLine("Adapter::.actor");
+        }
+
+        public static bool ProcessOrder(string orderId)
+        {
+            Console.WriteLine("Adapter::ProcessOrder(string)");
+            return true;
+        }
+    }
+
+    class ProcessorBase
+    {
+        public ProcessorBase()
+        {
+            Console.WriteLine("ProcessorBase::.actor");
+        }
+    }
+
+    class Processor : ProcessorBase
+    {
+        public Processor(string orderId)
+        {
+            Console.WriteLine("Processor::.actor(string)");
+            Adapter.ProcessOrder(orderId);
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Processor p = new Processor("G700");
+            Console.ReadLine();
+        }
+    }
+}
+
